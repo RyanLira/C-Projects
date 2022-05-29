5ef3b47693ea70414cda2572254b73ce3e4daa04
Ryan Lira <101383915+RyanLira@users.noreply.github.com>
2022-05-28 20:31:10 -0500

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,21 @@
+//Ryan Lira May 28, 2022
+//this was more of an exercise in the terminal than c++
+
+
+/* compile in terminal with the command
+ g++ fileName.cpp -o executibleName
+ then run the compiled executible code
+./executibleName
+u can also compile it into c code with gcc
+gcc fileName.c -o executibleName
+./executibleName
+rm to delete files in terminal
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "hello world" << endl;
+	return 0;
+}
