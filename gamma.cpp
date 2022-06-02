311a10543a9fa1833be96638a9f90cc7aed87706
Ryan Lira <101383915+RyanLira@users.noreply.github.com>
2022-06-02 16:29:49 -0500

    Add files via upload

---
+++ gamma.cpp
@@ -0,0 +1,22 @@
+//Ryan Lira May 2, 2022
+//this is a function which will go inside
+//a larger function so solve higher order
+//linear equations.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+float a;
+float b;
+
+cout << "a; ";
+cin >> a;
+cout << "b: ";
+cin >> b;
+cout << (-b)/(3*a) << endl;
+
+	return 0;
+}
